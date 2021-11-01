21d3bfed7da8d59da4d1b2be4581174f54463640
JasonSzMz <93508652+JasonSzMz@users.noreply.github.com>
2021-11-01 10:31:08 -0600

    Add files via upload

---
+++ Pseint-Calificaciones.psc
@@ -0,0 +1,10 @@
+Algoritmo Calificaciones
+	definir calificacion Como Entero
+	Escribir "Ingresa la califiación para determinar si es aprobatoria"
+	Leer calificacion
+	Si calificacion >= 7 Entonces
+		Escribir "La calificación es aprobatoria"
+	SiNo
+		Escribir "La calificación no es aprobatoria"
+	FinSi
+FinAlgoritmo
---
+++ calificación_Aprobatoria.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    
+    int calificacion;
+    printf("Ingresa la califiaci%cn para determinar si es aprobatoria\n",162);
+    printf("Calificaci%cn: ",162);
+    cin >> calificacion;
+    if (calificacion >= 7)
+    {
+        printf("\n\nLa calificaci%cn es aprobatoria",162);
+    }
+    else
+    {
+        printf("\n\nLa calificaci%cn no es aprobatoria",162);
+    
+    }
+}
